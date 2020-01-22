0c0b6b3db19d57d0e64a5d3b08a1cd8f58604617
hardik49 <hardik.thakar49@gmail.com>
2020-01-22 13:18:13 +0530

    replace forEach with findIndex in deleteUser()

--- actions.js
+++ actions.js
@@ -18,38 +18,37 @@ function message(statusCode, status, msg, data = '') {
 function addNewUser(userData) {
   fileContent.forEach((element) => {
     if (element.id == userData.id) {
-          return message(200, 'OK', 'Your user ID is already exists!');
-        } else if (element.email == userData.email) {
-          return message(200, 'OK', 'Your email is already exists!');
-        } else if (element.mobile == userData.mobile) {
-          return message(200, 'OK', 'Your mobile is already exists!');
+      return message(200, 'OK', 'Your user ID is already exists!');
+    } else if (element.email == userData.email) {
+      return message(200, 'OK', 'Your email is already exists!');
+    } else if (element.mobile == userData.mobile) {
+      return message(200, 'OK', 'Your mobile is already exists!');
+    }
+
+    if (element.id !== userData.id) {
+      if (element.email !== userData.email) {
+        if (element.mobile !== userData.mobile) {
+          flag = true;
         }
-    
-        if (element.id !== userData.id && element.email !== userData.email && element.mobile !== userData.mobile) {
-          userDetailsList.push(JSON.parse(userData));
-          flag = 1;
-        }
-    });
-  if (flag == 1) {
-    fs.writeFile('data.json', JSON.stringify(userDetailsList), function (err) {
-      if (err) throw err;
-    })
+      }
+    }
+  });
+  if (flag) {
+    userDetailsList.push(JSON.parse(userData));
+    fs.writeFileSync('data.json', JSON.stringify(userDetailsList));
     return message(200, 'OK', 'User successfully being added');
+  } else {
+    return message(200, 'OK', 'User exists');
   }
 }
 
 function deleteUser(userData) {
-  fileContent.forEach((element) => {
-    if (element.id == JSON.parse(userData).id) {
-      userDetailsList.splice(userDetailsList.indexOf(element, 1));
-      flag = 1;
-    }
-  });
-  if (flag == 1) {
-    fs.writeFile('data.json', JSON.stringify(userDetailsList), function (err) {
-      if (err) throw err;
-      return 'User deleted successfully!'
-    })
+  const deleteUser = userDetailsList.splice(userDetailsList.findIndex(() => {
+    return userData.id;
+  }), 1);
+  if (deleteUser) {
+    fs.writeFileSync('data.json', JSON.stringify(userDetailsList));
+    return 'User deleted successfully!';
   }
   else {
     return 'User Id does not  exists';
@@ -65,7 +64,6 @@ function viewUser(userData) {
       flag = 1;
     }
   });
-  
   if (flag) {
     return message(200, 'OK', 'User successfully being added', userId);
   } else {
@@ -74,33 +72,29 @@ function viewUser(userData) {
 }
 
 function updateUser(userData) {
-  
-fileContent.forEach((element) => {
-  if (element.id == JSON.parse(userData).id) {
-
-    Object.keys(JSON.parse(userData)).forEach((ele) => {
-      if (ele == "email") {
-        if (JSON.parse(userData).email != "") {
-          element.email = JSON.parse(userData).email;
+  fileContent.forEach((element) => {
+    if (element.id == JSON.parse(userData).id) {
+      Object.keys(JSON.parse(userData)).forEach((ele) => {
+        if (ele == "email") {
+          if (JSON.parse(userData).email != "") {
+            element.email = JSON.parse(userData).email;
+          }
         }
-      }
-      if (ele == "mobile") {
-        if (JSON.parse(userData).mobile != "") {
-          element.mobile = JSON.parse(userData).mobile;
+        if (ele == "mobile") {
+          if (JSON.parse(userData).mobile != "") {
+            element.mobile = JSON.parse(userData).mobile;
+          }
         }
-      }
-    }); 
-    flag = 1;
-  }
-});
+      });
+      flag = 1;
+    }
+  });
 
   if (flag == 1) {
-    fs.writeFile('data.json', JSON.stringify(userDetailsList), function (err) {
-      if (err) throw err;
-      return 'User Updated successfully!'
-    })
+    fs.writeFileSync('data.json', JSON.stringify(userDetailsList));
+    return 'User Updated';
   } else {
-    console.log('User does not exists');
+    return 'User does not exists';
   }
 }
 
--- index.js
+++ index.js
@@ -2,29 +2,44 @@ const http = require('http');
 const actions = require('./actions');
 const port = 8080;
 const host = '127.0.0.1';
-let body = '';
+
 const requestListener = function (req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/JSON');
-  req.on('data', function (data) {
-    body += data;
-  });
-  if (req.url === '/add') {
+  
+  if (req.url === '/add' && req.method === 'POST') {
+    let body = '';
+    req.on('data', function (data) {
+      body = data;
+    });
     req.on('end', function () {
       res.end(actions.addNewUser(body));
+   
     });
   }
-  if (req.url === '/delete') {
+  if (req.url === '/delete' && req.method == 'POST') {
+    let body = '';
+    req.on('data', function (data) {
+      body = data;
+    });
     req.on('end', function () {
       res.end(actions.deleteUser(body));
     });
   }
-  if (req.url === '/view') {
+  if (req.url === '/view' && req.method == 'GET') {
+    let body = '';
+    req.on('data', function (data) {
+      body = data;
+    });
     req.on('end', function () {
       res.end(actions.viewUser(body));
     });
   }
-  if (req.url === '/update') {
+  if (req.url === '/update' && req.method == 'POST') {
+    let body = '';
+    req.on('data', function (data) {
+      body = data;
+    });
     req.on('end', function () {
       res.end(actions.updateUser(body));
     });
